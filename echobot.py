0f44003d037511e9a59df40947fa44b9e1543179
chromsinx <chromsinx@gmail.com>
2024-10-16 19:30:49 +0300

    Add files via upload

---
+++ echobot.py
@@ -0,0 +1,13 @@
+import telebot
+# Создаем экземпляр бота
+bot = telebot.TeleBot('7049915362:AAGsFV9YQb_YCxCNzTm5p5iyRy4GQl4EGLI')
+# Функция, обрабатывающая команду /start
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
+# Получение сообщений от юзера
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
